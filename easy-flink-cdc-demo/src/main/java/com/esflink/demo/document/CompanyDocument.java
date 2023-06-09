5bb38e371694e57fda60caeb27241f4cc3485954
zhouhongyin <1429855087@qq.com>
2023-06-09 17:52:47 +0800

    demo 集成 es

--- easy-flink-cdc-demo/src/main/java/com/esflink/demo/document/CompanyDocument.java
+++ easy-flink-cdc-demo/src/main/java/com/esflink/demo/document/CompanyDocument.java
@@ -1,6 +1,10 @@
 package com.esflink.demo.document;
 
+import cn.easyes.annotation.IndexField;
+import cn.easyes.annotation.IndexId;
 import cn.easyes.annotation.IndexName;
+import cn.easyes.annotation.rely.Analyzer;
+import cn.easyes.annotation.rely.FieldType;
 import lombok.Data;
 
 /**
@@ -12,11 +16,13 @@ public class CompanyDocument {
     /**
      * id
      */
+    @IndexId
     private String id;
 
     /**
      * 公司名称
      */
+    @IndexField(fieldType = FieldType.KEYWORD_TEXT, analyzer = Analyzer.IK_MAX_WORD)
     private String name;
     /**
      * 公司简称
