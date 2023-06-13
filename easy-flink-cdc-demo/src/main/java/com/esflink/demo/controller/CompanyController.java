3529c78e4403ed7e11f7768c4e6397fb50a251bd
zhouhongyin <1429855087@qq.com>
2023-06-13 18:01:01 +0800

    es highlight

--- easy-flink-cdc-demo/src/main/java/com/esflink/demo/controller/CompanyController.java
+++ easy-flink-cdc-demo/src/main/java/com/esflink/demo/controller/CompanyController.java
@@ -1,32 +1,26 @@
 package com.esflink.demo.controller;
 
-import cn.easyes.core.conditions.LambdaEsQueryWrapper;
-import com.alibaba.fastjson.JSONObject;
-import com.esflink.demo.document.CompanyDocument;
 import com.esflink.demo.mapper.es.CompanyDocumentMapper;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestBuilder;
 import org.elasticsearch.search.suggest.SuggestBuilders;
-import org.elasticsearch.search.suggest.completion.CompletionSuggestionBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * @author zhouhongyin
@@ -53,15 +47,30 @@ public class CompanyController {
                         SuggestBuilders.completionSuggestion("keyword")
                                 .prefix(key).analyzer("ik_pinyin_analyzer").skipDuplicates(true).size(10)));
 
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<front color='red'>");
+        highlightBuilder.postTags("</front>");
+        highlightBuilder.field("keyword");
+        searchRequest.source().highlighter(highlightBuilder);
+
         SearchResponse search = companyDocumentMapper.search(searchRequest, RequestOptions.DEFAULT);
         Suggest suggest = search.getSuggest();
         List<? extends Suggest.Suggestion.Entry<? extends Suggest.Suggestion.Entry.Option>> entries = suggest.getSuggestion("suggest").getEntries();
 
-        for (Suggest.Suggestion.Entry<? extends Suggest.Suggestion.Entry.Option> entry: entries) {
+        for (Suggest.Suggestion.Entry<? extends Suggest.Suggestion.Entry.Option> entry : entries) {
             for (Suggest.Suggestion.Entry.Option option : entry.getOptions()) {
                 String keyword = option.getText().string();
                 rest.add(keyword);
             }
+        }
+
+
+        SearchHits hits = search.getHits();
+        for (SearchHit hit : hits) {
+            Map<String, HighlightField> name = hit.getHighlightFields();
+            HighlightField highlightField = name.get(name);
+            System.out.println(highlightField);
         }
         return String.join(",", rest);
     }
